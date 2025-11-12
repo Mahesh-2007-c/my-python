ae18ba4a8ad6da92aff8d239dc518363b673036b
Mahesh-2007-c <maheshchandrannavar3@gmail.com>
2025-11-12 11:12:20 +0530

    Create if elif else.py

---
+++ if elif else.py
@@ -0,0 +1,14 @@
+#exam result
+a = int(input("enter your marks"))
+if a>=95:
+  print("grade A+")
+elif a>=80:
+   print("grade A")
+elif a>=70:
+   print("grade B+")
+elif a>=60:
+   print("grade B")
+elif a==50:
+   print("grade C")
+else :
+  print("fail")
