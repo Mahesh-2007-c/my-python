1975175630eb739c8a9967d9ed6dc28d1cf1c407
Mahesh-2007-c <maheshchandrannavar3@gmail.com>
2025-11-12 11:14:46 +0530

    Create largest two number.py

---
+++ largest two number.py
@@ -0,0 +1,8 @@
+#largest two number
+num1 = int(input("enter the first number"))
+num2 = int(input("enter the second number"))
+if num1>num2:
+  print(num1,"is largest number")
+  print(num2,"is smalest number")
+else:
+  print(num1,"is a smalest number")
