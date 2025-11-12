109398378ccd13a1de637549e503dd0836ce4ae6
Mahesh-2007-c <maheshchandrannavar3@gmail.com>
2025-11-12 15:18:24 +0530

    Create function all.py

---
+++ function all.py
@@ -0,0 +1,16 @@
+def anyone(x,y):
+  name = input("enter your name")
+  age = int(input("enter your age"))
+  roll_no = int(input("enter your roll_no"))
+  addition=age+roll_no
+  print("the sum is:",sum)
+  mul=x*y
+  return name,age,roll_no
+  print("multiplication is",mul)
+  return name,age,roll_no
+anyone(5,10)  
+
+  
+
+
+
