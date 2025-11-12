26d7d922704a3741e016721072b972216e66f1dc
Mahesh-2007-c <maheshchandrannavar3@gmail.com>
2025-11-12 11:31:33 +0530

    Create loop.py

---
+++ loop.py
@@ -0,0 +1,9 @@
+num=int(input("enter number"))
+count=0
+while num!=0: 
+  num=num//10
+  count+=1
+print("number of digit in given number is",count)
+  
+enter number3546
+number of digit in given number is
