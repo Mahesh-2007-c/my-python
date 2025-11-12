ed56025712b0e04a74f5c680f1f7fb5a81f63db8
Mahesh-2007-c <maheshchandrannavar3@gmail.com>
2025-11-12 11:25:58 +0530

    Create formate string.py

---
+++ formate string.py
@@ -0,0 +1,6 @@
+#formated string
+name="mahesh"
+age=19
+collage="s i e t"
+departament="ai&ml"
+print(f"my name is: {name} my age is: {age} i am studing in: {collage} departament of: {departament}")
